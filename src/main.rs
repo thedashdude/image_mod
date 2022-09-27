2b5d565170b7d1ddf7722bb1d9824025175ba110
thedashdude <thedashdude@yahoo.com>
2022-09-27 18:21:29 -0500

    Removed legacy code
    Also got rid of all compiler complaints

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-//#![allow(dead_code)]
-//#![allow(unused_variables)]
-//#![allow(unused_imports)]
-
 use std::env;
 use image::{GenericImageView,Pixel};
 
@@ -10,8 +6,8 @@ use image::{GenericImageView,Pixel};
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	if args.len() < 2 {
-		panic!("Too few arguments")
+	if args.len() < 3 || args.len() > 3 {
+		panic!("Wrong number of arguments")
 	}
 	if args.len() == 3 
 	{
@@ -21,19 +17,19 @@ fn main() {
 		let file_name_dest = &args[2];
 
 		let img_dynamic_result = image::open(file_name_source);
-		if let Err(e) = img_dynamic_result {
+		if let Err(_) = img_dynamic_result {
 			println!("Failed to open file '{}', terminating...", file_name_source);
 			return;
 		}
 		let img_src = img_dynamic_result.unwrap().to_rgb8();
 
 		let (width, height) = img_src.dimensions();
-		let num_pixels: i64 = (width as i64) * (height as i64);
+		let _num_pixels: i64 = (width as i64) * (height as i64);
 
 
 
 		let img_dynamic_result = image::open(file_name_dest);
-		if let Err(e) = img_dynamic_result {
+		if let Err(_) = img_dynamic_result {
 			println!("Failed to open file '{}', terminating...", file_name_dest);
 			return;
 		}
@@ -78,7 +74,7 @@ fn main() {
 
 		println!("\n\nSaving to 'out.png'");
 		let save_result = img_dest.save("out.png");
-		if let Err(e) = save_result {
+		if let Err(_) = save_result {
 			println!("Failed to save file, terminating...");
 			return;
 		}
@@ -102,7 +98,7 @@ fn apply_dist_functional<F,G>(img: &mut image::RgbImage, get_stat: F, set_stat: 
 {
 	let (old_mean, old_sd) = get_dist_functional(&img, &get_stat);
 	//Naive approach
-	for (x,y,pixel) in img.enumerate_pixels_mut()
+	for (_x,_y,pixel) in img.enumerate_pixels_mut()
 	{
 		let old_stat = get_stat(pixel);
 		let new_stat: f32 = (old_stat - old_mean)/old_sd * new_sd + new_mean;
@@ -149,77 +145,6 @@ fn get_stat_sd<F>(img: &image::RgbImage, get_stat: F, mean: f32) -> f32 where
 	let variance: f32 = dev_avg_sum / (width as f32);
 	let sd: f32 = variance.sqrt();
 	sd
-}
-
-fn apply_luma_dist_functional(img: &mut image::RgbImage, new_mean: f32, new_sd: f32) -> () {
-	let (width, height) = img.dimensions();
-	let num_pixels: i64 = (width as i64) * (height as i64);
-	let (old_mean, old_sd) = get_luma_mean_sd(&img);
-	//Naive approach
-	for (x,y,pixel) in img.enumerate_pixels_mut()
-	{
-		let old_luma = get_luma(pixel);
-		let new_luma = (old_luma - old_mean)/old_sd * new_sd + new_mean;
-		set_luma(pixel, new_luma);
-		
-		//let (r,g,b) = (pixel[0],pixel[1],pixel[2]);
-		//let adjust = |x| { ((x as f32 - old_mean)*new_sd/old_sd + new_mean) as u8};
-		//*pixel = image::Rgb([adjust(r),adjust(g),adjust(b)]);
-		////img.put_pixel(x,y,image::Rgb([1,2,3]));
-	}
-}
-
-fn apply_luma_dist(img: &mut image::RgbImage, new_mean: f32, new_sd: f32) -> () {
-	let (width, height) = img.dimensions();
-	let num_pixels: i64 = (width as i64) * (height as i64);
-	let (old_mean, old_sd) = get_luma_mean_sd(&img);
-	//Naive approach
-	for (x,y,pixel) in img.enumerate_pixels_mut()
-	{
-		let (r,g,b) = (pixel[0],pixel[1],pixel[2]);
-		let adjust = |x| { ((x as f32 - old_mean)*new_sd/old_sd + new_mean) as u8};
-		*pixel = image::Rgb([adjust(r),adjust(g),adjust(b)]);
-		//img.put_pixel(x,y,image::Rgb([1,2,3]));
-	}
-}
-
-fn get_luma_mean(img: &image::RgbImage) -> f32 {
-	let (width, height) = img.dimensions();
-	let mut col_avg_sum: f32 = 0.0;
-	for n in 0..width 
-	{
-		let v = img.view(n,0,1,height);
-		let mut col_sum: i32 = 0;
-		for (_x,_y,pixel) in v.pixels() {
-			col_sum += pixel.to_luma()[0] as i32;
-		}
-		col_avg_sum += (col_sum as f32) / (height as f32);
-	}
-	let avg: f32 = col_avg_sum / (width as f32);
-	avg
-}
-
-fn get_luma_sd(img: &image::RgbImage, mean: f32) -> f32 {
-	let (width, height) = img.dimensions();
-	let mut dev_avg_sum: f32 = 0.0;
-	for n in 0..width 
-	{
-		let v = img.view(n,0,1,height);
-		let mut col_dev_sum: f32 = 0.0;
-		for (_x,_y,pixel) in v.pixels() {
-			col_dev_sum += (mean - (pixel.to_luma()[0] as f32)).powf(2.0);
-		}
-		dev_avg_sum += (col_dev_sum as f32) / (height as f32);
-	}
-	let variance: f32 = dev_avg_sum / (width as f32);
-	let sd: f32 = variance.sqrt();
-	sd
-}
-
-fn get_luma_mean_sd(img: &image::RgbImage) -> (f32, f32){
-	let m = get_luma_mean(img);
-	let s = get_luma_sd(img, m);
-	(m,s)
 }
 
 fn get_luma(pixel: &image::Rgb<u8>) -> f32 {
